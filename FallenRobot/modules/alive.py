4701a497fa10e3e76178df635548d67c4ae1a47e
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-15 10:12:25 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -12,7 +12,7 @@ from FallenRobot import telethn as tbot
 
 PHOTO = [
     "https://telegra.ph/file/728d7a3793ba5695e5465.jpg",
-    "https://telegra.ph/file/728d7a3793ba5695e5465.jpg",
+    "https://telegra.ph/file/7907401bc31e7721fb0f0.jpg",
 ]
 
 @register(pattern=("/alive"))
@@ -22,7 +22,7 @@ async def awake(event):
   TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/AARUXRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/AARUSUPPORT")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/AaRu_X_RoBoT?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/AARU_SUPPORT")]]
   ran = random.choice(PHOTO)
   await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
 
